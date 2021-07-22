23bb35c5f1fa2dec688ce9b264b63cd9bf9f5642
Paula <paula@MacBookdeMacBook-Air.local>
2021-07-21 23:55:51 -0500

    video y nombre centrado

---
+++ src/components/cover/Cover.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Cover.css"
+import covervideo from "../../media/coverVideo.mp4"
+const Cover = () => {
+    
+    return (
+        <div className="cover-container">
+            <video className="video" src={covervideo} autoPlay lopp muted />
+            <h1>Paulo Perez Paternina</h1>
+            <p>Developer | Entrepeneur</p>
+        </div>
+    );
+};
+
+export default Cover
