abe5851d209517cf16d37bd4a984291dec5a11d9
Paula <paula@MacBookdeMacBook-Air.local>
2021-07-24 16:27:40 -0500

    Modulo about con texto Lorem

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,7 +5,10 @@ const About = () => {
         <div className="about-container"> 
             <div className="about-desc">
                 <h3>About me</h3>
-                <p>Text</p>
+                <p>Text Text TextTextTextTextTextTextTex
+                    tTextTextTextTextTextTextTextTextT
+                    extTextTextTextTextTextTextTextTextT
+                    extTextTextTextTextTextText</p>
             </div>
             <div className="about-img">
                 <img src="https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="About" />
