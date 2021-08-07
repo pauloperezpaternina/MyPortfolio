8ad6d6c9e18ed8bccb939f9c2bda67553cbddb2d
Paula <paula@MacBookdeMacBook-Air.local>
2021-08-06 22:23:30 -0500

    about ready

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,11 +4,8 @@ const About = () => {
     return (
         <div className="about-container"> 
             <div className="about-desc">
-                <h3>About me</h3>
-                <p>Text Text TextTextTextTextTextTextTex
-                    tTextTextTextTextTextTextTextTextT
-                    extTextTextTextTextTextTextTextTextT
-                    extTextTextTextTextTextText</p>
+                <h3>Acerca de mi</h3>
+                <p>Desarrollador entusiasta, me gusta aprender y enseñar lo aprendido, experiencia con tecnologias como Python, React y Java.</p>
             </div>
             <div className="about-img">
                 <img src="https://images.pexels.com/photos/879109/pexels-photo-879109.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="About" />
